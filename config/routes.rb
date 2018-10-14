c6891e9b35cefb5bc39463f83c8325043ed7ef3a
llamaeyes <llama@siliconvalerie.com>
2018-10-14 12:09:28 -0700

    updates for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'rsvps#index'
   resources :rsvps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
